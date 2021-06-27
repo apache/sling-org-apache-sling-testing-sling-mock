17dd6d00991ab95d793af357dfff0495af6fd7da
Stefan Seifert <stefanseifert@users.noreply.github.com>
2021-06-27 14:19:10 +0200

    cosmetic: switch to hamcrest assertThat to avoid deprecation warning

--- core/src/test/java/org/apache/sling/testing/mock/sling/services/MockSlingSettingServiceTest.java
+++ core/src/test/java/org/apache/sling/testing/mock/sling/services/MockSlingSettingServiceTest.java
@@ -19,8 +19,8 @@
 package org.apache.sling.testing.mock.sling.services;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
