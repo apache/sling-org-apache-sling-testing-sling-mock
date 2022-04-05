02f0037bd9a7907d6a930e52a145d2c359b04a9d
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-04-05 15:52:19 +0200

    SLING-11252 increase package version

--- core/src/main/java/org/apache/sling/testing/mock/sling/package-info.java
+++ core/src/main/java/org/apache/sling/testing/mock/sling/package-info.java
@@ -19,5 +19,5 @@
 /**
  * Mock implementation of selected Sling APIs.
  */
-@org.osgi.annotation.versioning.Version("3.3.0")
+@org.osgi.annotation.versioning.Version("3.4.0")
 package org.apache.sling.testing.mock.sling;
