9627d024dd0fe858d8411bb1825eb4641dfc4200
Radu Cotescu <170911+raducotescu@users.noreply.github.com>
2022-05-05 18:06:00 +0200

    SLING-11297 - The org.apache.sling.testing.mock.sling.RRMockResourceResolverWrapper should first query its providers (#12)
    * implemented the rest of the methods that read resources
    * added tests
    
    Co-authored-by: Stefan Seifert <stefanseifert@users.noreply.github.com>

--- junit4/src/test/java/org/apache/sling/testing/mock/sling/junit/SlingContextDefaultRRTypeTest.java
+++ junit4/src/test/java/org/apache/sling/testing/mock/sling/junit/SlingContextDefaultRRTypeTest.java
@@ -18,8 +18,11 @@
  */
 package org.apache.sling.testing.mock.sling.junit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.apache.sling.api.resource.Resource;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -31,6 +34,24 @@ public class SlingContextDefaultRRTypeTest {
     @Test
     public void testRequest() {
         assertNotNull(context.request());
+    }
+
+    @Test
+    public void testResourceOperationsOnMountedFolder() {
+        String root = context.uniqueRoot().content() + "/test-content";
+        context.load().folderJson("src/test/resources/test-content", root);
+
+        Resource parent = context.resourceResolver().getResource(root + "/parent");
+        assertNotNull( "Expected to resolve the 'parent' resource.", parent);
+        assertNotNull("Expected to resolver the 'child' resource.", parent.getChild("child"));
+
+        Resource uniqueRoot = context.resourceResolver().getParent(parent);
+        assertNotNull("Expected to resolve the unique root.", uniqueRoot);
+        assertEquals("The resolved unique root is not identical to the created unique root.", root, uniqueRoot.getPath());
+
+        assertTrue("Expected to see a list of children.", context.resourceResolver().listChildren(parent).hasNext());
+        assertTrue("Expected to get a list of children.", context.resourceResolver().getChildren(parent).iterator().hasNext());
+        assertTrue("Expected to see a list of children.", parent.hasChildren());
     }
 
 }
