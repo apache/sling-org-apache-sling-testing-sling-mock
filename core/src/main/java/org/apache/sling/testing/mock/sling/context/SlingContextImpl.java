7a8706a9024dede95d94c9a348815863a120f048
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-08-15 11:21:05 +0200

    SLING-12002 Discard "special logging" to stdout from ESAPI (#29)

--- core/src/main/java/org/apache/sling/testing/mock/sling/context/SlingContextImpl.java
+++ core/src/main/java/org/apache/sling/testing/mock/sling/context/SlingContextImpl.java
@@ -77,6 +77,11 @@ import org.osgi.service.event.EventHandler;
 @ConsumerType
 public class SlingContextImpl extends OsgiContextImpl {
 
+    static {
+        // ugly hack: prevent default console logging from ESAPI on each initialization by setting system property
+        System.setProperty("org.owasp.esapi.logSpecial.discard", "true");
+    }
+
     // default to publish instance run mode
     static final @NotNull Set<String> DEFAULT_RUN_MODES = Collections.singleton("publish");
 
